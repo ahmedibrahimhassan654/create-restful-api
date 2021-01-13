e1ef20ec456f8b5c9b274044b685ea8abd6ecbe7
Ahmed Ibrahim <39348867+ahmedibrahimhassan654@users.noreply.github.com>
2021-01-14 00:27:35 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var http=require('http')
+
+
+const server =http.createServer(function(req, res){
+    res.end('server is working')
+})
+
+
+
+ server.listen(3000,function(){
+     console.log('listening on port 3000')
+ })
