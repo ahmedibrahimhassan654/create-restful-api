ad920d4b3f6264502b69ed6b511bbc8c7873dbd0
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2021-01-14 15:10:00 +0200

    lec 13 finished

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 var http=require('http')
 var url=require('url')
-//the server 
+//the server should respond to all the request with string
 const server =http.createServer(function(req, res){
-    res.end('server is working')
+
+    //get the url and parse it
+    var parseUrl=url.parse(req.url,true)
+
+
+    //get the path
+    var path=parseUrl.pathname//this path is not trimed
+
+    //trim the path 
+    var trimedPath=path.replace(/\\/g, '')
+
+
+    
+    //send the response
+    res.end('hello world\n')
+
+    console.log('request recive at path',trimedPath);
+
+
+
+     
 })
-
-var parseUrl=url.parse(req.url,true)
-
-var path=parseUrl.pathname
 
  server.listen(3000,function(){
      console.log('listening on port 3000')
