d96affee0b34c2ea88033cb729ca0eb5211bde3c
ahmedibrahimhassan654 <ahmedibrahimhassan654@gmail.com>
2021-01-14 11:41:52 +0200

    create server

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 var http=require('http')
-
-
+var url=require('url')
+//the server 
 const server =http.createServer(function(req, res){
     res.end('server is working')
 })
 
+var parseUrl=url.parse(req.url,true)
 
+var path=parseUrl.pathname
 
  server.listen(3000,function(){
      console.log('listening on port 3000')
-     console.log('listening on port 3000')
-
-
-
     })
